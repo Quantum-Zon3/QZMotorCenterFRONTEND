0a925d18902e8ac5fadd2aedbbbee60b72533b7f
CarlosMPatino7822 <carlosmariopatinoramirez@gmail.com>
2026-05-24 20:58:49 -0500

    Mas endpoints

--- src/features/auth/auth.api.ts
+++ src/features/auth/auth.api.ts
@@ -2,13 +2,13 @@ import { authClient, authProtectedClient } from "../../lib/http/clients";
 import type { AuthResponse, AuthUser, LoginPayload, RegisterPayload } from "./auth.types";
 
 export const registerRequest = async (payload: RegisterPayload) => {
-  const { data } = await authClient.post<AuthUser>("/auth/register", payload);
+  const { data } = await authClient.post<AuthUser>("/qzMotorCenter/auth", payload);
   return data;
 };
 
 export const loginRequest = async (payload: LoginPayload) => {
   const { data } = await authClient.post<AuthResponse>(
-    "/auth/login",
+    "/qzMotorCenter/auth/login",
     payload,
   );
   return data;
@@ -16,12 +16,12 @@ export const loginRequest = async (payload: LoginPayload) => {
 
 export const refreshTokenRequest = async (refreshToken: string) => {
   const { data } = await authClient.post<AuthResponse>(
-    "/auth/refresh-token",
+    "/qzMotorCenter/auth/refresh-token",
     { refreshToken },
   );
   return data;
 };
 
 export const logoutRequest = async () => {
-  await authProtectedClient.post("/auth/logout");
+  await authProtectedClient.post("/qzMotorCenter/auth/logout");
 };
